c0bf50b18307110261144d049eace00532ea03c7
thynkon <thynkon@protonmail.com>
2020-11-29 12:46:37 +0100

    Install intel driver on laptop mode

--- scripts/graphics-card.sh
+++ scripts/graphics-card.sh
@@ -1,15 +1,55 @@
 #!/usr/bin/env bash
 #set -e
 
-source "${HOME}/programming/arcolinux-setup/lib/lib.sh"
+source "${HOME}/programming/archlinux-setup/lib/lib.sh"
 
-aurPackages=(
-    gwe
-)
+mode=$1
 
-archPackages=(
-    nvidia
-)
+aurPackages=()
+archPackages=()
+
+setup_brightness() {
+cat > /etc/X11/xorg.conf.d/20-intel.conf << EOL
+Section "Device"
+    Identifier  "Intel Graphics"
+    Driver      "intel"
+    Option      "Backlight"  "intel_backlight"
+EndSection
+EOL
+}
+export -f setup_brightness
+
+case "${mode}" in
+    desktop)
+        aurPackages+=(gwe)
+        archPackages+=(nvidia)
+        ;;
+    laptop)
+        archPackages+=(xf86-video-intel)
+        ;;
+    *)
+        echo "Mode ${mode} does not exist!!!"
+        echo "Valid modes: desktop or laptop"
+        echo "Exiting..."
+
+        exit 1
+        ;;
+esac
 
 installAurPackages "${aurPackages[@]}"
 installArchPackages "${archPackages[@]}"
+
+case "${mode}" in
+    desktop)
+        ;;
+    laptop)
+        su -c setup_brightness
+        ;;
+    *)
+        echo "Mode ${mode} does not exist!!!"
+        echo "Valid modes: desktop or laptop"
+        echo "Exiting..."
+
+        exit 1
+        ;;
+esac
